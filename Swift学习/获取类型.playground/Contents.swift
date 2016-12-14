4c0c91ce54120f5a437010107f954c549725d1fc
y.j.zhang <y.j.zhang@medilink.com.cn>
2016-12-14 16:06:05 +0800

    获取Type

---
+++ Swift学习/获取类型.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let s0 = UIViewController.self
+
+let s00 = s0.init()
+let s01 = UIViewController.self()
+let s02 = UIViewController()
+
+
+let s1 = type(of: UIViewController())
+
+let s10 = s1.init()
+
+
+let s2 = UIViewController().classForCoder
